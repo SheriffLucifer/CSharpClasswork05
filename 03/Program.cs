406f0df7f74d185514445ac18b4cdefe425f3963
Oleg <olegashepelev@gmail.com>
2023-01-17 21:05:34 +0600

    Complete third task

---
+++ 03/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int[] array = GetRandomArray(123, -100, 123);
+int numberElements = FindNumberElements(array);
+Console.WriteLine($"[{String.Join(", ", array)}] -> {numberElements}");
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return result;
+}
+
+int FindNumberElements(int[] array)
+{
+    int numbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if ((array[i] >= 10) && (array[i] <= 99)) numbers++;
+    }
+    return numbers;
+}
